90266cc773fb4c975c68f3d8b45cc9e8c0ea6241
The-EleetCoder <jaijain1803@gmail.com>
2023-09-23 14:46:23 +0530

    created diff sections

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+const Button = ({ label, iconURL }) => {
+  return (
+    <button className="flex justify-center items-center bg-coral-red px-7 py-4 rounded-full text-white text-lg leading-none font-montserrat gap-2 border border-coral-red">
+      {label}
+      <img
+        src={iconURL}
+        alt="arrow right icon"
+        className="ml-2 rounded-full w-5 h-5"
+      />
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/sections/index.js
@@ -0,0 +1,12 @@
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+
+export {
+    Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer
+}
